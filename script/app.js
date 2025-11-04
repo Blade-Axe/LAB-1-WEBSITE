aa409825bdbe573891da7407e82e3c18abacf706
BladeAxe <Madaxam@protonmail.com>
2025-11-04 14:18:51 +0000

    added fetch javascript to external file

--- script/app.js
+++ script/app.js
@@ -46,3 +46,49 @@ if(document.getElementById("myForm")){
     });
 }
 
+if(document.getElementById("schedule")){
+    let mySchedule = document.getElementById("schedule");
+    let localJsonFile = "/data.json";
+    let sectionElement = document.querySelector('section');
+
+    document.addEventListener('DOMContentLoaded', ()=>{
+        fetch(localJsonFile)
+        .then(response => response.json())
+        .then (responseData =>{ console.log(responseData);
+            for (item of responseData){
+                const schedule = document.createElement("article");
+                const imageElement = document.createElement("img");
+                const level4heading = document.createElement("h4");
+                const para1 = document.createElement("p");
+                const para2 = document.createElement("p");
+                const para3 = document.createElement("p");
+                const para4 = document.createElement("p");
+
+                schedule.classList.add("services");
+
+                sectionElement.appendChild(schedule)
+                imageElement.src = item.imageURL;
+                imageElement.alt = item.alt;
+                schedule.appendChild(imageElement);
+
+                level4heading.textContent = item.style;
+                schedule.appendChild(level4heading);
+
+                para1.textContent = item.time;
+                schedule.appendChild(para1);
+
+                para2.textContent = item.focus;
+                schedule.appendChild(para2);
+
+                para3.textContent = item.benefits;
+                schedule.appendChild(para3);
+
+                para4.textContent = item.level;
+                schedule.appendChild(para4);
+
+                
+            }  
+        })
+        .catch(error => console.error("Error fetching JSON data:", error));
+    })
+}
